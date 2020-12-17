b45535e6b68c0dc3e4941541769d4305da1e7d54
苦艾酒 <37876529+JAPL666@users.noreply.github.com>
2020-12-18 01:53:46 +0800

    哔哩哔哩

--- src/main/java/com/warma/bilibili/MyTest.java
+++ src/main/java/com/warma/bilibili/MyTest.java
@@ -12,7 +12,8 @@ public class MyTest {
         String host_uid="281120836";
         String offset_dynamic_id="0";
 
-        while (true){
+        boolean bool=true;
+        while (bool){
             String url="https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?visitor_uid=29204204&host_uid="+host_uid+"&offset_dynamic_id="+offset_dynamic_id+"&platform=web";
             HashMap<String, String> result = Warma.get(url, new HashMap<>());
 
@@ -31,14 +32,18 @@ public class MyTest {
                     JSONObject desc = cards.getJSONObject("desc");
 
                     if(desc.toString().contains("dynamic_id_str")){
-                        //动态ID
-                        String dynamic_id = desc.getString("dynamic_id_str");
-
                         //动态的时间戳
                         long timestamp = desc.getLong("timestamp");
-                        long time = System.currentTimeMillis();
+                        long time = System.currentTimeMillis()/1000;
                         long x=time-timestamp;
-                        System.out.println("时间："+x);
+                        long x1=((60*60)*24)*30;
+                        if(x>x1){
+                            System.out.println("时间超过一个月");
+                            bool=false;
+                        }
+
+                        //动态ID
+                        String dynamic_id = desc.getString("dynamic_id_str");
 
                         if(i==cardArray.length()-1){
                             System.out.println(dynamic_id);
