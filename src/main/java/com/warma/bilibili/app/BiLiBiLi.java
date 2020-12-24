b1f70daad675cb118376085945c3bf05b7b1e0d8
苦艾酒 <37876529+JAPL666@users.noreply.github.com>
2020-12-25 01:59:16 +0800

    哔哩哔哩

--- src/main/java/com/warma/bilibili/app/BiLiBiLi.java
+++ src/main/java/com/warma/bilibili/app/BiLiBiLi.java
@@ -29,34 +29,42 @@ public class BiLiBiLi {
         List<BiLiBiLiInfoEntity> userInfo = biLiBiLi.service.findUserInfo();
 
         if(userInfo!=null){
+            //遍历多个用户
             for (BiLiBiLiInfoEntity biLiBiLiInfoEntity : userInfo) {
 
+                //循环次数
                 for (int i = 0; i < 100; i++) {
 
+                    //随机生成UID
                     int randomUid = Warma.Random(2, 9000000);
+                    //通过UID获取用户信息
                     BiLiBiLiInfoEntity info = biLiBiLiApi.getInfo(randomUid);
                     if(info.getCode()==-412){
+                        //请求繁忙，请求被拦截
                         System.out.println("请求被拦截>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
                     }else if(info.getCode() == 0){
+
+                        //等级大于0才执行
                         if(info.getLevel()>0){
 
                             ArrayList<BiLiBiLiEntity> dynamicIdList = biLiBiLiApi.getDynamicIdList(String.valueOf(info.getUid()));
                             if(dynamicIdList.size()!=0){
-                                System.out.println("名字："+info.getName());
-                                System.out.println("UID："+info.getUid());
-                                System.out.println("等级："+info.getLevel());
-                                System.out.println("性别："+info.getSex());
-                                System.out.println("头像："+info.getFace());
 
                                 for (BiLiBiLiEntity biLiBiLiEntity : dynamicIdList) {
                                     System.out.println("动态ID："+biLiBiLiEntity.getDynamicId());
+                                    //获取关注状态
                                     boolean followed = biLiBiLiApi.is_followed(biLiBiLiInfoEntity, String.valueOf(info.getUid()));
+                                    //如果没有关注
                                     if(!followed){
+                                        //关注
                                         biLiBiLiApi.modify(biLiBiLiInfoEntity,String.valueOf(biLiBiLiEntity.getHost_uid()),1);
                                     }
                                     BiLiBiLiEntity entity = new BiLiBiLiEntity();
+                                    //要转发动态的ID
                                     entity.setDynamicId(biLiBiLiEntity.getDynamicId());
+                                    //自己的UID
                                     entity.setMyuid(String.valueOf(biLiBiLiInfoEntity.getUid()));
+                                    //转发抽奖动态
                                     biLiBiLiApi.dynamic_repost(biLiBiLiInfoEntity,entity,"我收下了、、、、、");
 
                                 }
