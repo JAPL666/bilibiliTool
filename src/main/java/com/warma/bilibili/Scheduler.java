b9223b718601d2d0efbf6d9a2495c365111201a3
苦艾酒 <37876529+JAPL666@users.noreply.github.com>
2020-12-26 03:28:32 +0800

    哔哩哔哩

--- src/main/java/com/warma/bilibili/Scheduler.java
+++ src/main/java/com/warma/bilibili/Scheduler.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class Scheduler {
 
     //定时获取抽奖动态并转发
-    @Scheduled(cron = "0 0 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * ? ")
+    @Scheduled(cron = "0 0 * * * ? ")
     public void getDynamic(){
         new BiLiBiLi().getDynamic();
     }
